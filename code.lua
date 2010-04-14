13d350ee64696adcd2014d65a774f0ea085ca8b5
John Cramp <john@ice.local>
2010-04-14 00:58:47 -0700

    restore grid's old 'by class' layout

---
+++ code.lua
@@ -0,0 +1,26 @@
+
+--frame:RegisterEvent("GUILD_ROSTER_UPDATE")
+--frame:RegisterAllEvents()
+
+local squeenixFixer = CreateFrame("Frame")
+squeenixFixer:RegisterEvent("UNIT_PORTRAIT_UPDATE")
+squeenixFixer:SetScript("OnEvent",
+	function(self, event, ...)
+		Minimap:SetMaskTexture("Interface\\AddOns\\Squeenix\\Mask.blp")
+	end
+)
+
+-- restore old grid "By Class" layout
+GridLayout:AddLayout("By Class w/Pets", {
+	[1] = { groupFilter = "WARRIOR", },
+	[2] = { groupFilter = "DEATHKNIGHT", },
+	[3] = { groupFilter = "ROGUE", },
+	[4] = { groupFilter = "PALADIN", },
+	[5] = { groupFilter = "DRUID", },
+	[6] = { groupFilter = "SHAMAN", },
+	[7] = { groupFilter = "PRIEST", },
+	[8] = { groupFilter = "MAGE", },
+	[9] = { groupFilter = "WARLOCK", },
+	[10] = { groupFilter = "HUNTER", },
+	[11] = { isPetGroup = true, },
+})
