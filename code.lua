de924f3ef730d334f5b67b9375e2d547247e6203
John Cramp <john@ice.local>
2010-10-14 20:50:09 -0700

    disable whisper stickyness; disable grid layout

--- code.lua
+++ code.lua
@@ -10,17 +10,23 @@ squeenixFixer:SetScript("OnEvent",
 	end
 )
 
+-- disable /w stickyness
+ChatTypeInfo.WHISPER.sticky = 0
+
 -- restore old grid "By Class" layout
-GridLayout:AddLayout("By Class w/Pets", {
-	[1] = { groupFilter = "WARRIOR", },
-	[2] = { groupFilter = "DEATHKNIGHT", },
-	[3] = { groupFilter = "ROGUE", },
-	[4] = { groupFilter = "PALADIN", },
-	[5] = { groupFilter = "DRUID", },
-	[6] = { groupFilter = "SHAMAN", },
-	[7] = { groupFilter = "PRIEST", },
-	[8] = { groupFilter = "MAGE", },
-	[9] = { groupFilter = "WARLOCK", },
-	[10] = { groupFilter = "HUNTER", },
-	[11] = { isPetGroup = true, },
-})
+--GridLayout:AddLayout("By Class w/Pets", {
+--	[1] = { groupFilter = "WARRIOR", },
+--	[2] = { groupFilter = "DEATHKNIGHT", },
+--	[3] = { groupFilter = "ROGUE", },
+--	[4] = { groupFilter = "PALADIN", },
+--	[5] = { groupFilter = "DRUID", },
+--	[6] = { groupFilter = "SHAMAN", },
+--	[7] = { groupFilter = "PRIEST", },
+--	[8] = { groupFilter = "MAGE", },
+--	[9] = { groupFilter = "WARLOCK", },
+--	[10] = { groupFilter = "HUNTER", },
+--	[11] = { isPetGroup = true, },
+--})
+
+-- hmm so the icon comes back when you change tabs
+-- what's the event for that?
