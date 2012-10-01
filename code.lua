0b37fe724e7deee19736cdfceeb72f7ce00ac84c
John Cramp <john>
2012-09-30 19:12:21 -0700

    no taint here...

--- code.lua
+++ code.lua
@@ -262,10 +262,6 @@ end
 
 
 -- hide the minimap blob ring (so that's what it's called!)
--- XXX it's possible this is causing taint(?)
--- map in combat
--- WatchFrame_Update() from jQuest?
--- http://www.wowinterface.com/forums/showthread.php?t=32859
 Minimap:SetArchBlobRingScalar(0)
 Minimap:SetQuestBlobRingScalar(0)
 
