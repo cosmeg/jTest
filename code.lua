eafb392436339acab1667688b0c0faac85be0f3b
John Cramp <john>
2012-12-05 12:27:43 -0800

    fix /jt to work on all toons

--- code.lua
+++ code.lua
@@ -285,36 +285,24 @@ hooksecurefunc("QuestPOI_DisplayButton", FadePOIButton)
 
 -- slash commands to toggle various tracking
 -- jt = j track
+local TRACKING  = { }
+for index = 1,GetNumTrackingTypes() do
+  local name, _ = GetTrackingInfo(index)
+  TRACKING[name] = index
+  --print(index.." "..name)
+end
 SLASH_JTEST_JT1 = "/jt"
 function SlashCmdList.JTEST_JT(msg, editbox)
-  --[[
-  local count = GetNumTrackingTypes()
-  for index = 1,count do
-    local name, texture, active, category = GetTrackingInfo(index)
-    print(index.." "..name)
-  end
-  --]]
 
   if msg == "pet" or msg == "pets" then
     -- battle pets
-    local index = 1
+    local index = TRACKING["Track Pets"]
     local name, _, active, _ = GetTrackingInfo(index)
-
-    -- TODO remove these failsafes if it works on all toons
-    if name ~= "Track Pets" then
-      print("WARNING! Just toggled "..name..", which is probably not what you wanted.")
-    end
-
     SetTracking(index, not active)
 
   elseif msg == "mail" or msg == "mailbox" then
-    local index = 9
+    local index = TRACKING["Mailbox"]
     local name, _, active, _ = GetTrackingInfo(index)
-
-    if name ~= "Mailbox" then
-      print("WARNING! Just toggled "..name..", which is probably not what you wanted.")
-    end
-
     SetTracking(index, not active)
   end
 end
