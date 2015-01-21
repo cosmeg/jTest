23d931a74f7a11a9d4902034e180019623753e81
John Cramp <jbc.git@gmail.com>
2015-01-20 20:56:41 -0800

    move /jt commands to jMap

--- code.lua
+++ code.lua
@@ -232,31 +232,6 @@ end
 hooksecurefunc("QuestPOI_DisplayButton", FadePOIButton)
 
 
--- slash commands to toggle various tracking
--- jt = j track
-local TRACKING  = { }
-for index = 1,GetNumTrackingTypes() do
-  local name, _ = GetTrackingInfo(index)
-  TRACKING[name] = index
-  --print(index.." "..name)
-end
-SLASH_JTEST_JT1 = "/jt"
-function SlashCmdList.JTEST_JT(msg, editbox)
-
-  if msg == "pet" or msg == "pets" then
-    -- battle pets
-    local index = TRACKING["Track Pets"]
-    local name, _, active, _ = GetTrackingInfo(index)
-    SetTracking(index, not active)
-
-  elseif msg == "mail" or msg == "mailbox" then
-    local index = TRACKING["Mailbox"]
-    local name, _, active, _ = GetTrackingInfo(index)
-    SetTracking(index, not active)
-  end
-end
-
-
 -- say something in chat when the dungeon queue pops
 local queueAlerter = CreateFrame("Frame")
 queueAlerter:SetScript("OnEvent", function(self, ...) print(...) end)
